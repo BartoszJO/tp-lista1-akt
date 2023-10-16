26abaeafc2926f90f8d521dbbc7376db420355b5
BartoszJO <bartoszjolszewski@hotmail.com>
2023-10-16 18:58:05 +0200

    First Commit

--- src/main/java/technologiaprogramowania/ClassB.java
+++ src/main/java/technologiaprogramowania/ClassB.java
@@ -8,4 +8,3 @@ public class ClassB {
         return "To jest klasa ClassB";
     }
 }
-//
--- src/test/java/technologiaprogramowania/MainTest.java
+++ src/test/java/technologiaprogramowania/MainTest.java
@@ -18,4 +18,3 @@ public class MainTest {
         assertEquals("To jest klasa ClassB", messageB);
     }
 }
-//
